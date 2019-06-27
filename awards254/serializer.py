3ef49bf54b84fa2c58b15cdb38de7813a8918b17
josephmuema <jmuema95@gmail.com>
2019-06-27 11:57:10 +0300

    added a class for the project serializer

--- awards254/serializer.py
+++ awards254/serializer.py
@@ -2,3 +2,12 @@ from rest_framework import serializers
 from .models import *
 from django.contrib.auth.models import User
 
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['title','description','image_path','owner','website_link']
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['user','bio','myproject','profile_image','email']
