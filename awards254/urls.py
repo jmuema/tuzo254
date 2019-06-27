3bf0f6bed1e0f65ac69aea1ca5960dc4270c412f
josephmuema <jmuema95@gmail.com>
2019-06-27 12:07:00 +0300

    created the url patterns

---
+++ awards254/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from .import views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
