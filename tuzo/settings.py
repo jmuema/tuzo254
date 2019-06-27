0f39ba71fb68a474941f90be5f59908e684aaf72
josephmuema <jmuema95@gmail.com>
2019-06-27 12:25:20 +0300

    Added my database

--- tuzo/settings.py
+++ tuzo/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'tuzo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tuzo',
+        'USER': 'joseph',
+        'PASSWORD': '25MuemA25',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
