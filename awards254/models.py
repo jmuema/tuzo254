380e3156e0f14a847fab48219e5aa59242ba777c
josephmuema <jmuema95@gmail.com>
2019-06-27 15:42:10 +0300

    fixed an error on the profile class

--- awards254/models.py
+++ awards254/models.py
@@ -1,23 +1,20 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Project(models.Models):
-  title = models.CharField(max_length = 50)
-  description = models.CharField(max_length =100)
-  image_path = models.ImageField(Upload_to='project_images/')
-  owner = models.ForeignKey(User,on_delete=models.CASCADE)
-  website_link = models.CharField(max_length =100)
-  
-  @classmethod
-  def search_by_title(cls,title):
-    searched= cls.objects.filter(title_icontains=title)
-    return searched
+class Project(models.Model):
+    title = models.CharField(max_length =50)
+    description = models.CharField(max_length =100)
+    image_path = models.ImageField(upload_to='project_images/')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    website_link= models.CharField(max_length =100)
+    
+    @classmethod
+    def search_by_title(cls,title):
+        searched= cls.objects.filter(title__icontains=title)
+        return searched
 
-  def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)    #one user has one profile
@@ -37,3 +34,5 @@ class Review(models.Model):
     review = models.TextField(max_length=100,null=True)
     project = models.ForeignKey(Project)
     
+
+
