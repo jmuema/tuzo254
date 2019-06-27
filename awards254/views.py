bb0e754bd7911b1d2fb579854bbb7fe62cca2d56
josephmuema <jmuema95@gmail.com>
2019-06-27 11:45:00 +0300

    imported django shortcuts to fix an error

--- awards254/views.py
+++ awards254/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from . forms import *
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from .serializer import ProjectSerializer, ProfileSerializer
 
 # Create your views here.
