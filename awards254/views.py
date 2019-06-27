78f2a7b119aabd6f69646cc09fcc9dcbad9ec2ff
josephmuema <jmuema95@gmail.com>
2019-06-27 11:47:05 +0300

    added a function tohelp with registration

--- awards254/views.py
+++ awards254/views.py
@@ -8,3 +8,14 @@ from rest_framework.views import APIView
 from .serializer import ProjectSerializer, ProfileSerializer
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
