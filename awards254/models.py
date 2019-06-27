e2670fb7fe2d5706a29e816c9fa8bd47ed69aa19
josephmuema <jmuema95@gmail.com>
2019-06-27 11:26:17 +0300

    added a class for the profile

--- awards254/models.py
+++ awards254/models.py
@@ -18,3 +18,14 @@ class Project(models.Models):
 
   def __str__(self):
     return self.title
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)    #one user has one profile
+    bio= models.CharField(max_length =100, default="Digital eye candy")
+    myproject = models.ForeignKey(Project)
+    profile_image= models.ImageField(default='default.jpeg')
+    email= models.CharField(max_length =50)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+        
